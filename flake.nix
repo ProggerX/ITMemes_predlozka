43581a6714cb8f053853512c206242f33c1ef437
ProggerX <x@proggers.ru>
2023-12-17 13:16:37 +0300

    Initial

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = {self, nixpkgs, flake-utils} :
+	flake-utils.lib.eachDefaultSystem (system: 
+		let pkgs = import nixpkgs { inherit system; };
+		in {
+			packages.default = pkgs.stdenv.mkDerivation {
+				name = "ITMemes-predloz";
+
+				nativeBuildInputs = with pkgs; [
+					gcc
+					bear
+				];
+				buildInputs = with pkgs; [
+					boost.dev
+					bzip2
+					zlib
+					openssl
+				];
+
+				buildPhase = "just build";
+				installPhase = "cp out $out";
+			};
+		}
+	);
+}
